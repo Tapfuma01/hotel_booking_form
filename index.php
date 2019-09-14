ead6697a1e4e2bae8d8bf5c56bd292f0bbab16ab
Tapfuma <enosmhundwa@gmail.com>
2019-09-14 16:18:26 +0200

    Update index.php
    removed unwanted text

--- index.php
+++ index.php
@@ -34,81 +34,12 @@ session_start();
         <!--HOTEL IMAGES AND PRICES-->
     <div class="container">
             <div class="row text-">
-              <div class="col-md-12">
-               <!--Carousel Wrapper-->
-<div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
-        <!--Indicators-->
-        <ol class="carousel-indicators">
-          <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
-          <li data-target="#carousel-example-1z" data-slide-to="1"></li>
-          <li data-target="#carousel-example-1z" data-slide-to="2"></li>
-          <li data-target="#carousel-example-1z" data-slide-to="3"></li>
-        </ol>
-        <!--/.Indicators-->
-        <!--Slides-->
-        <div class="carousel-inner" role="listbox">
-          <!--First slide-->
-          <div class="carousel-item active">
-            <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(141).jpg" alt="First slide">
-          </div>
-          <!--/First slide-->
-          <!--Second slide-->
-          <div class="carousel-item">
-            <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(136).jpg" alt="Second slide">
-          </div>
-          <!--/Second slide-->
-          <!--Third slide-->
-          <div class="carousel-item">
-            <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(137).jpg" alt="Third slide">
-          </div>
-          <!--/Third slide-->
-          <div class="carousel-item">
-            <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(136).jpg" alt="Second slide">
-          </div>
-        </div>
-        <!--/.Slides-->
-        <!--Controls-->
-        <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-        <!--/.Controls-->
-      </div>
-      <!--/.Carousel Wrapper-->
-       <!--Card content-->
-       <div class="card-body card-body-cascade text-center">
-            <!--Title-->
-            <h4 class="card-title"><strong>PROTEA HOTEL</strong></h4>
-            <h5 class="indigo-text"><strong>R1200/nite</strong></h5>
-          </div>
-              </div>
-              
-                  </div>
-                </div>
-              </div>
-             
-              <!--HOTEL USER INPUT FIELD-->
-              <h2 class="text-center">BOOK HERE</h2>
-  
-    <div class="container">
-  <!--Section: Live preview-->
-<section class="form-dark">
-
-<!--Form without header-->
-<div class="card card-image"
-  style="background-image: url('https://image.freepik.com/free-photo/woman-reception-hotel-checking_95419-4849.jpg'); width: 28rem;">
-  <div class="text-white rgba-stylish-strong py-5 px-5 z-depth-4">
-
-    <!--Header-->
-    <div class="text-center">
-      <h3 class="white-text mb-5 mt-4 font-weight-bold"><strong>BOOKING</strong> <a
-          class="green-text font-weight-bold"><strong> FORM</strong></a></h3>
-    </div>
-
+            <div class="col-md-6">
+            </div>
+            <div class="col-md-6">
+            </div>
+            </div> 
+    </div> 
     <!--Body-->
     <div class="md-form">
       <input type="text" id="Form-email5" class="form-control white-text">

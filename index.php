6c315ed37f6390f811d8c4812f02e4314f3c9325
Tapfuma <enosmhundwa@gmail.com>
2019-09-18 07:59:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/styles.css">
     <link href='https://fonts.googleapis.com/css?family=Fjalla One' rel='stylesheet'>
     
     
